2d164af04221e5d65ded09a1b87f4232fa93098b
Lucas <lcavalcantidearaujo@gmail.com>
2024-10-30 21:54:54 -0300

    class ProductController

---
+++ src/main/java/com/example/product_catalog/controllers/ProductController.java
@@ -0,0 +1,12 @@
+package com.example.product_catalog.controllers;
+
+import com.example.product_catalog.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+}
