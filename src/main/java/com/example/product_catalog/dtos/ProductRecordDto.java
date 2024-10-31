9573409dd9d5f78fedcace100ef7d04d697c0ba8
Lucas <lcavalcantidearaujo@gmail.com>
2024-10-30 22:01:27 -0300

    record ProductRecordDto

---
+++ src/main/java/com/example/product_catalog/dtos/ProductRecordDto.java
@@ -0,0 +1,9 @@
+package com.example.product_catalog.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
+}
